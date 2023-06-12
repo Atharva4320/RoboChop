ed9beb9d0580bcabfb2c44137b3f54dd031beb83
Alison Bartsch <abartsch@andrew.cmu.edu>
2023-06-12 17:13:44 -0400

    added udpcomms

--- frankapy_centroid.py
+++ frankapy_centroid.py
@@ -1,44 +1,58 @@
 import argparse
+import time
 from utils import *
 from frankapy import FrankaArm
 from perception import CameraIntrinsics
 from UDPComms import Subscriber, timeout, Scope
 # import warning
 
-# # import camera intrinsics and extrinsics
+# import camera intrinsics and extrinsics
+REALSENSE_INTRINSICS = "vision_module/2D_vision/calib/realsense_intrinsics_camera4.intr"
+REALSENSE_EE_TF = "vision_module/2D_vision/calib/realsense_camera4.tf"
 # REALSENSE_INTRINSICS = "vision_module/2D_vision/calib/realsense_intrinsics.intr"
 # REALSENSE_EE_TF = "vision_module/2D_vision/calib/realsense_ee_shifted.tf"
-# parser = argparse.ArgumentParser()
-# parser.add_argument(
-#     "--intrinsics_file_path", type=str, default=REALSENSE_INTRINSICS
-# )
-# parser.add_argument("--extrinsics_file_path", type=str, default=REALSENSE_EE_TF)
-# args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--intrinsics_file_path", type=str, default=REALSENSE_INTRINSICS
+)
+parser.add_argument("--extrinsics_file_path", type=str, default=REALSENSE_EE_TF)
+args = parser.parse_args()
 
-# realsense_intrinsics = CameraIntrinsics.load(args.intrinsics_file_path)
-# realsense_to_ee_transform = RigidTransform.load(args.extrinsics_file_path)
+realsense_intrinsics = CameraIntrinsics.load(args.intrinsics_file_path)
+realsense_to_ee_transform = RigidTransform.load(args.extrinsics_file_path)
 
-# fa = FrankaArm()
+print("\nReset pose...")
+fa = FrankaArm()
+fa.reset_pose()
+fa.reset_joints()
+pose = fa.get_pose()
+pose.translation = np.array([0.5, 0, 0.5])
+print("\nGo to observation pose...")
+fa.goto_pose(pose)
 
-sub = Subscriber(5501, timeout=0.2)
+sub = Subscriber(5502, timeout=0.2)
 
 while True:
 	try: 
 		message = sub.get()
-		print(message)
+		if message == 'No centroid Detected!':
+			print(message)
+		else:
+			x = message[0][0]
+			y = message[0][1]
+			z = message[0][2]
 
-		# x = message[0][0]
-		# y = message[0][1]
-		# z = message[0][2]
+			com = get_object_center_point_in_world_realsense_static_camera(np.array([x,y,z]), realsense_intrinsics, realsense_to_ee_transform)
 
-		# robot_pose = fa.get_pose()
-		# com = get_object_center_point_in_world_realsense_3D_camera_point(np.array([x,y,z]), realsense_intrinsics, realsense_to_ee_transform, robot_pose)
+			robot_pose = fa.get_pose()
+			# com = get_object_center_point_in_world_realsense_3D_camera_point(np.array([x,y,z]), realsense_intrinsics, realsense_to_ee_transform, robot_pose)
 
-		# # --------- FINAL 3D POINT IN FRANKA WORLD FRAME ----------
-		# com = np.array([com[0], com[1], com[2]]) # should be the x,y,z position in robot frame
-		# print("\nCOM: ", com)
-		# robot_pose.translation = np.array([com[0], com[1], com[2] + 10])
-		# # fa.goto_pose(robot_pose)
+			# --------- FINAL 3D POINT IN FRANKA WORLD FRAME ----------
+			com = np.array([com[0], com[1] - 0.05, com[2]]) # should be the x,y,z position in robot frame
+			print("COM: ", com)
+			robot_pose.translation = np.array([com[0], com[1], com[2] + 0.4])
+			# fa.goto_pose(robot_pose)
+			# time.sleep(5)
 
 	except timeout:
 		print("no message")
