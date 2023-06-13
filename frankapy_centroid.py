c4f22613a030e7d15d350db820ed3bef42e3c620
Alison Bartsch <abartsch@andrew.cmu.edu>
2023-06-13 19:10:28 -0400

    Apple can be cut!!!

--- frankapy_centroid.py
+++ frankapy_centroid.py
@@ -8,10 +8,10 @@ import UdpComms as U
 # import warning
 
 # import camera intrinsics and extrinsics
-REALSENSE_INTRINSICS = "vision_module/2D_vision/calib/realsense_intrinsics_camera4.intr"
-REALSENSE_EE_TF = "vision_module/2D_vision/calib/realsense_camera4.tf"
-# REALSENSE_INTRINSICS = "vision_module/2D_vision/calib/realsense_intrinsics.intr"
-# REALSENSE_EE_TF = "vision_module/2D_vision/calib/realsense_ee_shifted.tf"
+# REALSENSE_INTRINSICS = "vision_module/2D_vision/calib/realsense_intrinsics_camera4.intr"
+# REALSENSE_EE_TF = "vision_module/2D_vision/calib/realsense_camera4.tf"
+REALSENSE_INTRINSICS = "vision_module/2D_vision/calib/realsense_intrinsics.intr"
+REALSENSE_EE_TF = "vision_module/2D_vision/calib/realsense_ee_shifted.tf"
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "--intrinsics_file_path", type=str, default=REALSENSE_INTRINSICS
@@ -22,14 +22,14 @@ args = parser.parse_args()
 realsense_intrinsics = CameraIntrinsics.load(args.intrinsics_file_path)
 realsense_to_ee_transform = RigidTransform.load(args.extrinsics_file_path)
 
-# print("\nReset pose...")
-# fa = FrankaArm()
-# fa.reset_pose()
-# fa.reset_joints()
-# pose = fa.get_pose()
-# pose.translation = np.array([0.5, 0, 0.5])
-# print("\nGo to observation pose...")
-# fa.goto_pose(pose)
+print("\nReset pose...")
+fa = FrankaArm()
+fa.reset_pose()
+fa.reset_joints()
+reset_pose = fa.get_pose()
+reset_pose.translation = np.array([0.55, 0, 0.6]) # x was 0.55 before
+print("\nGo to observation pose...")
+fa.goto_pose(reset_pose)
 
 # dictionary of camera serials
 # 1: '220222066259',
@@ -49,36 +49,34 @@ while True:
 		message = udp.ReadReceivedData()
 		if message is None:
 			continue
-		# if message == 'No centroid Detected!':
-		# 	continue
-		# # if message is ''
-		# message = sub.get()
-		# print(udp.rxThread)
-		# udp.ReadUdpThreadFunc()
-		# print(message.split(','))
-		x = float(message.split(',')[0][2:-1])
+
+		print(message)
+
+		x = float(message.split(',')[0].split('[')[2])
 		y = float(message.split(',')[1])
 		z = float(message.split(',')[2].split(']')[0])
-		# print(np.array([x,y,z]))
-		# if message == 'No centroid Detected!':
-		# 	print(message)
-		# else:
-		# 	x = message[0][0]
-		# 	y = message[0][1]
-		# 	z = message[0][2]
 
-		com = get_object_center_point_in_world_realsense_static_camera(np.array([x,y,z]), realsense_intrinsics, realsense_to_ee_transform)
+		# com = get_object_center_point_in_world_realsense_static_camera(np.array([x,y,z]), realsense_intrinsics, realsense_to_ee_transform)
+		# print("COM: ", com)
+
+		robot_pose = fa.get_pose()
+		com = get_object_center_point_in_world_realsense_3D_camera_point(np.array([x,y,z]), realsense_intrinsics, realsense_to_ee_transform, robot_pose)
+		# print("COM: ", com)
+
+		# --------- FINAL 3D POINT IN FRANKA WORLD FRAME ----------
+		com = np.array([com[0] - 0.015, -com[1] + 0.02, com[2] + 0.02]) # should be the x,y,z position in robot frame
 		print("COM: ", com)
+		robot_pose.translation = np.array([com[0], com[1], com[2] + 0.11])
 
-		# 	robot_pose = fa.get_pose()
-		# 	# com = get_object_center_point_in_world_realsense_3D_camera_point(np.array([x,y,z]), realsense_intrinsics, realsense_to_ee_transform, robot_pose)
+		fa.goto_pose(robot_pose)
+		time.sleep(5)
 
-		# 	# --------- FINAL 3D POINT IN FRANKA WORLD FRAME ----------
-		# 	com = np.array([com[0], com[1] - 0.05, com[2]]) # should be the x,y,z position in robot frame
-		# 	print("COM: ", com)
-		# 	robot_pose.translation = np.array([com[0], com[1], com[2] + 0.4])
-		# 	# fa.goto_pose(robot_pose)
-		# 	# time.sleep(5)
+		fa.goto_gripper(0, block=False)
+		fa.apply_effector_forces_along_axis(1.0, 0.5, 0.06, forces=[0.,0.,-75.])
+
+		print("\nGo to observation pose...")
+		fa.goto_pose(reset_pose)
+		break
 
 	except timeout:
 		print("no message")
@@ -92,3 +90,12 @@ while True:
 	# 	# warning.warn("UDPComms timeout")
 	# 	print("UDPComss timeout")
 	# 	break
+
+
+
+	"""
+	Offset Estimates (in cm):
+	x: 2, 1, +3
+	y: 3, 4, 1
+	z: 1, 0.5, 2
+	"""
