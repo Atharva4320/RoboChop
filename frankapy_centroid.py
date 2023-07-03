df12fc868276a1cdf7d5757de83ae0a569555c9c
Alison Bartsch <abartsch@andrew.cmu.edu>
2023-07-03 17:43:27 -0400

    updated skill library to include mutliclass logic with improved additional skills

--- frankapy_centroid.py
+++ frankapy_centroid.py
@@ -10,15 +10,30 @@ import re
 from numpy import array
 from skill_utils import *
 
+
+# --------- DEFINE TARGETS HERE --------
+target_classes = ['Apple']
+
 # updIP: This computer, SendIP: other computer 
 udp = U.UdpComms(udpIP='172.26.5.54', sendIP='172.26.69.200', portTX=5501, portRX=5500, enableRX=True)
+
+# send message of the target classes to detect
+object_string = ''
+for key in target_classes:
+	object_string+=key
+	object_string+=','
+object_string = object_string[0:len(object_string)-1]
+print("object_string: ", object_string)
+
+udp.SendData(object_string)
+time.sleep(20) # TODO: determine how long we need to sleep here
 
 print("\nReset pose...")
 fa = FrankaArm()
 fa.reset_pose()
 fa.reset_joints()
 reset_pose = fa.get_pose() # [0.65, 0, 0.4]
-reset_pose.translation = np.array([0.625, 0, 0.45]) # x was 0.55 before
+reset_pose.translation = np.array([0.625, 0, 0.58]) # x was 0.45 before
 
 skills = SkillUtils(fa)
 obs_objects, obj_dict = skills.observe_scene(udp, reset_pose)
