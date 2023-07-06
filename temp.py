b6d3dae9d7ba36d1a113b8c4dddcdd7e34fea661
Alison Bartsch <abartsch@andrew.cmu.edu>
2023-07-06 16:47:01 -0400

    added to temp to reset and go to observation pose

--- temp.py
+++ temp.py
@@ -1,6 +1,9 @@
 from frankapy import FrankaArm
+import numpy as np
 
 fa = FrankaArm()
-pose = fa.get_pose()
-print(pose)
-fa.open_gripper()
+fa.reset_pose()
+fa.reset_joints()
+reset_pose = fa.get_pose()
+reset_pose.translation = np.array([0.65, 0, 0.58])
+fa.goto_pose(reset_pose)
